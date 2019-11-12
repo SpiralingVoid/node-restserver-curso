a69137c3d74779e7496580657a91a96593865e01
Abraham <j.abraham22.06@gmail.com>
2019-11-12 15:18:33 -0600

    RestServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,23 @@
 //puerto
 //===============================
 process.env.PORT = process.env.PORT || 3000;
+
+//===============================
+//Entorno
+//===============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//===============================
+//Entorno
+//===============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+
+} else {
+    urlDB = 'mongodb+srv://cafe_user:staytech22@cluster0-9r8om.mongodb.net/cafe?retryWrites=true&w=majority';
+
+}
+process.env.URLDB = urlDB;
